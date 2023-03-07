77abb40bcf9a4a55db823784fffaf983acc84521
Maria <115710550+Maria-TV@users.noreply.github.com>
2023-03-07 10:30:05 +0300

    Update lesson9_CourseImpl.java

--- lesson9_CourseImpl.java
+++ lesson9_CourseImpl.java
@@ -1,14 +1,1 @@
-package lesson9;
 
-public class CourseImpl implements Course {
-    private String name;
-
-    public CourseImpl(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
-}
